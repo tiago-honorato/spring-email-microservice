310617176bcf4271c5c37953472b8500fb85005f
Tiago Honorato <tiagohirute@gmail.com>
2023-12-01 17:49:06 -0300

    initial commit

---
+++ src/test/java/com/ms/email/MyMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
